dc1118b53c08efb185406fb1e4d8f61132256a26
NaveenNarayanan-P <pnaveen200@gmail.com>
2022-12-18 18:14:39 +0530

    assignment2 added

---
+++ assignment2.py
@@ -0,0 +1,32 @@
+# # access element from the list
+# lst = [1,2,3,"Hi",[45,54, "iNeuron"], "Big Data"]
+# print(lst[4][2])
+
+# #get a list from the user and print its length
+# lst1 = input("Enter something\n").split()
+# print(len(lst1))
+
+#Geek for Geek
+
+# number of elements
+# n = int(input("Enter number of elements : "))
+  
+# # Below line read inputs from user using map() function 
+# a = list(map(int,input("\nEnter the numbers : ").strip().split()))[1:n]
+  
+# print("\nList is - ", a)
+
+#list of list [['a',1],['b',2]]
+
+# n = int(input("Enter the number of list"))
+# lst2 = []
+# for i in range(0,n):
+#     ele = [input('Enter your name\n'), int(input("Enter your age"))]
+#     lst2.append(ele)
+
+# print(lst2)
+
+# add word big to third in the list
+# lst2 = ["Welcome", "to", "Data", "course"]
+# lst2 = lst2[:2] + ["big"] + lst2[2:]
+# print(lst2)
--- practice.py
+++ practice.py
@@ -1,21 +1,29 @@
-l1 = [0, 2, 1, 1, 0, 1, 2, 2,4]
-#acending order
-l1.sort()
-print(l1)
+# l1 = [0, 2, 1, 1, 0, 1, 2, 2,4]
+# #acending order
+# l1.sort()
+# print(l1)
 
-sl1 = set(l1)
-l2 = list(sl1)
-print(l2)
+# sl1 = set(l1)
+# l2 = list(sl1)
+# print(l2)
 
-count = 0 
-al3 = []
+# count = 0 
+# al3 = []
 
-for j in l2:
-    for i in l1: 
-        if i == j:
-            count += 1
-    al3.append(count)
-    count = 0
+# for j in l2:
+#     for i in l1: 
+#         if i == j:
+#             count += 1
+#     al3.append(count)
+#     count = 0
 
-for num in range(len(al3)):
-    print("Ocu of ",l2[num], " = ", al3[num])
+# for num in range(len(al3)):
+#     print("Ocu of ",l2[num], " = ", al3[num])
+
+string = "Big Data iNeuron"
+print(string[9:])
+print(string[-1:9:-1])
+print(string[::-1])
+print(string.replace(string, ""))
+print(string)
+print("'iNeuron\'s Big Data Course'")
